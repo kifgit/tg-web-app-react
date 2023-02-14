3e95324d19114fdcd275798d41867c9a1a0a8601
kifgit <kashinif@gmail.com>
2023-02-14 16:12:05 +0500

    initial2

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import './App.css';
 const tg = window.Telegram.WebApp;
 
 
+class APP extends Comment{
+    render(){
+      function App() {
+        useEffect(()=>{
+          tg.ready();
+        },[])
 
-function App() {
-
-  useEffect(()=>{
-    tg.ready();
-  },[])
-
-  const onClose=()=>{
-    tg.close()
+    const onClose=()=>{
+      tg.close()
+    }
+    return (
+      <div className="App">
+        work
+        <button onClick={onClose}>Закрыть</button>
+      </div>
+    );
   }
-  return (
-    <div className="App">
-      work
-      <button onClick={onClose}>Закрыть</button>
-    </div>
-  );
+  }
 }
-
 export default App;
