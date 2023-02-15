05ad3d6bfbea4e483db28df5d96b3464f2ec836a
kifgit <kashinif@gmail.com>
2023-02-15 14:34:31 +0500

    v2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>{user?.username}</span>
+            <span className={'username'}>ssss{user?.username}</span>
         </div>
     );
 };
