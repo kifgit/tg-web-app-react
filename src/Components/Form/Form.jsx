67b49fa55ce7840bc5176f0ef24dc9f303c6838b
kifgit <kashinif@gmail.com>
2023-02-15 16:16:42 +0500

    v2

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,9 +1,39 @@
 import React from "react";
- 
+import './Form.css' ;
 const Form = ()=> {
+    const [country,setCountry]=useState('');
+    const [city,setStreet]=useState('');
+    const [subject,setSubject]=useState('');
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+
     return (
-        <div>
-            FORM
+        <div className={"form"}>
+            <input className={"input"} 
+                type="text" 
+                placeholder={"Страна"}
+                value={country}
+                onChange={onChangeCountry}/>
+
+            <input className={"input"} 
+                type="text" 
+                placeholder={"Улица"}
+                value={street}
+                onChange={onChangeStreet}/>
+
+            <select value={subject} onChange={onChangeSubject} className={"select"}>
+                <option value={'physical'}>Физ. Лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
         </div>
     )
 }
